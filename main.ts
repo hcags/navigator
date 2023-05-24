2f771dcc96b2e2b36dcebfd627ff033ed77b4f10
hcags <120849679+hcags@users.noreply.github.com>
2023-05-24 15:01:21 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,66 @@
-
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    if (!(Navigate)) {
+        Richtung += 1
+        ShowDirection(Richtung)
+    } else {
+        Nextdiection()
+    }
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+	
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    if (!(Navigate)) {
+        let list: number[] = []
+        let Richtungen: number[] = []
+        Richtungen[list.length] = Richtung
+        basic.setLedColor(0xffff00)
+        basic.pause(300)
+        basic.setLedColor(0xff0000)
+    }
+})
+function ShowDirection (DIRECTION: number) {
+    if (DIRECTION == 1 || DIRECTION == 5) {
+        Richtung = 1
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (DIRECTION == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (DIRECTION == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (DIRECTION == 4) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+}
+function Nextdiection () {
+	
+}
+let Richtung = 0
+let Navigate = false
+let I = 0
+Navigate = false
+Richtung = 0
+basic.setLedColor(0xff0000)
