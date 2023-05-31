b8dbce3e8e1dfd65a8076e469a5df93433fbcc5f
hcags <120849679+hcags@users.noreply.github.com>
2023-05-31 14:53:31 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,23 +3,25 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
         richtung += 1
         ShowDirection(richtung)
     } else {
-        NextDirection()
+        nextDirection()
     }
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-	
+    if (navigate) {
+        navigate = true
+        basic.setLedColor(0x00ff00)
+    }
+    i = 0
+    nextDirection()
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     if (!(navigate)) {
         richtungen[richtungen.length] = richtung
-        basic.setLedColor(0x00ff00)
+        basic.setLedColor(0xffff00)
         basic.pause(300)
         basic.setLedColor(0xff0000)
     }
 })
-function NextDirection () {
-	
-}
 function ShowDirection (Direction2: number) {
     if (Direction2 == 1 || Direction2 == 5) {
         richtung = 1
@@ -56,10 +58,25 @@ function ShowDirection (Direction2: number) {
             `)
     }
 }
-let richtung = 0
+function nextDirection () {
+    if (richtungen.length > i) {
+        ShowDirection(richtungen[0])
+        i += 1
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # #
+            . # # # #
+            . . . # #
+            `)
+    }
+}
 let richtungen: number[] = []
+let richtung = 0
 let navigate = false
 let i = 0
+i = 0
 navigate = false
-richtungen += 0
+richtung = 0
 basic.setLedColor(0xff0000)
